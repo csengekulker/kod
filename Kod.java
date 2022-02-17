8b99508d208d912ae64dea9950d5b969d7be88f5
csengekulker <k71608883429@ktch.hu>
2022-02-17 10:41:11 +0100

    added .gitignore, loop throws error

--- Kod.java
+++ Kod.java
@@ -4,6 +4,17 @@ class Kod {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
-    System.out.println("Balogh Csenge");
+    System.out.println("Balogh Csenge\nSzoft_I_N\n2022.02.17");
+
+    int[] codes = {2248, 1834, 3823};
+
+    System.out.print("Adja meg a kódot: ");
+    int inputCode = sc.nextInt();
+
+    int i = 1;
+    while (i <= codes.length) {
+      System.out.println(codes[i]);
+      i++;
+    }
   }
 }
